bcc5648698e30b2e803269ea5b0950b2a3c18374
David <david.vitora@gmail.com>
2016-10-18 21:44:34 -0200

    Verificação Simples

--- src/main/java/com/damage/grafos/armazenamento/Definicoes.java
+++ src/main/java/com/damage/grafos/armazenamento/Definicoes.java
@@ -5,7 +5,10 @@
  */
 package com.damage.grafos.armazenamento;
 
+import com.damage.grafos.Aresta;
 import com.damage.grafos.Grafo;
+import com.damage.grafos.No;
+import com.damage.grafos.estruturasdedados.VectorNo;
 
 /**
  *
@@ -14,11 +17,62 @@ import com.damage.grafos.Grafo;
 public class Definicoes {
     
     public static String completo(Grafo grafo){
-        return "Completo";
+        boolean simples;
+        if(!simples(grafo).equalsIgnoreCase("Sim")){
+            return "Não";
+        }
+
+        return "Sim";
     }
     
-    public static  String simples(Grafo grafo){
-        return "Simples";
+    public static String simples(Grafo grafo){
+        boolean simples = true;
+        Aresta aresta;
+        No no, noVerifica;
+        VectorNo<No> nos;
+        
+        for(int i = 0; i < grafo.getAresta().size(); i++){
+            aresta = (Aresta) grafo.getAresta().get(i);
+            if(aresta.getNo1().equals(aresta.getNo2())){
+                return "Não";
+            }
+        }
+        
+        for(int i = 0; i < grafo.getNo().size(); i++){
+            nos = new VectorNo();
+            no  = grafo.getNo().getNo(i);
+            for(int j = 0; j < grafo.getAresta().size(); j++){
+                if(grafo.getAresta().getAresta(j).getNo1().equals(no)){
+                    nos.add(grafo.getAresta().getAresta(j).getNo2());
+                }
+                
+            }
+            
+            for(int j = 0; j < grafo.getAresta().size(); j++){
+                if(grafo.getAresta().getAresta(j).getNo2().equals(no)){
+                    nos.add(grafo.getAresta().getAresta(j).getNo1());
+                }
+                
+            }
+            
+            for(int j = 0; j < nos.size(); j++){
+                System.out.println(nos.getNo(j).getId());
+            }
+            
+            System.out.println("----------");
+            
+            for(int j = 0; j < nos.size(); j++){
+                for(int l = j + 1; l < nos.size(); l++){
+                    if(nos.getNo(j).equals(nos.getNo(l))){
+                        return "Não";
+                    }
+                }
+            }
+            
+            
+        }
+        
+        return "Sim";
     }
     
     public static  String planar(Grafo grafo){
